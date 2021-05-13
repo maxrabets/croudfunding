31c59e0e73f31e1564868bc8a30e636035118da1
maxrabets <rabetsmaxim@gmail.com>
2021-05-14 00:13:06 +0300

    Heroku prepared43

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ if(process.env.NODE_ENV === "production") {
   app.use(express.static(`${__dirname}/frontend/build`));
   app.get('*', (req, res) => {
     res.sendFile(`${__dirname}/frontend/build/index.html`);
-  });
+  });  
 }
 
 sequelize.sync({force: false}).then(async (result)=>{
