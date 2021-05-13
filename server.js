e94f6631380b9d40cdbf2bedb80f8f5c4a03f873
maxrabets <rabetsmaxim@gmail.com>
2021-05-13 22:50:21 +0300

    Index added

--- server.js
+++ server.js
@@ -23,12 +23,15 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-if(process.env.NODE_ENV === "production") {  
-  app.use(express.static(`${__dirname}/frontend/build`));
-}
-
 app.use("/campaigns", camapignsRouter);
 app.use("/users", usersRouter); 
+
+if(process.env.NODE_ENV === "production") {  
+  app.use(express.static(`${__dirname}/frontend/build`));
+  app.get('*', (req, res) => {
+    res.sendFile(`${__dirname}/frontend/build/index.html`);
+  });
+}
 
 sequelize.sync({force: false}).then(async (result)=>{
 /////////////////
